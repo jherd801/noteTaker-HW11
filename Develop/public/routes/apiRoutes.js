c746616f3d04a86e15d71763dfc6510c5bfd62dc
jherd801 <jherd801@gmail.com>
2020-05-08 12:47:49 -0600

    Wrote successful app.post code to save new notes to the db, verified that data persists

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -20,4 +20,9 @@ module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
       res.json(dbData);
     });
+
+    app.post("/api/notes", function(req, res) {
+        dbData.push(req.body);
+        res.json(true)
+    });
 };
